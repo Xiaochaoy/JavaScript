bb83d1312a391650d66a3261f7296bdd3205d04a
Xiaochao <chaoshixiaochao@gmail.com>
2023-10-24 13:56:13 +0200

    pelea

--- eje17-ChuckNorris/js/main.js
+++ eje17-ChuckNorris/js/main.js
@@ -1,22 +1,60 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
 let contenido = document.getElementById("contenido");
-const boton = document.getElementById("next");
-if (boton) {
-    boton.addEventListener("click", function () {
-        axios_1.default.get('https://api.chucknorris.io/jokes/random')
-            .then(response => {
-            const chiste = response.data.value;
-            if (contenido) {
-                contenido.innerHTML = chiste;
+const botonNext = document.getElementById("next");
+const categoriasDiv = document.getElementById("categorias");
+let cate = "";
+let selectedButton = null;
+let loquebuscas = document.getElementById("search");
+const botonbuscar = document.getElementById("buscar");
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+fetch("https://api.chucknorris.io/jokes/categories")
+    .then(response => response.json())
+    .then(data => {
+    data.forEach((categoria) => {
+        const boton = document.createElement("button");
+        boton.textContent = categoria;
+        categoriasDiv.appendChild(boton);
+        boton.addEventListener("click", () => {
+            if (selectedButton) {
+                selectedButton.classList.remove("select");
             }
-        })
-            .catch(error => {
-            console.error('Error al obtener el chiste:', error);
+            cate = categoria;
+            boton.classList.add("select");
+            selectedButton = boton;
         });
+    });
+})
+    .catch(error => console.log('error:', error));
+if (botonNext) {
+    botonNext.addEventListener("click", function () {
+        if (cate === "") {
+            fetch("https://api.chucknorris.io/jokes/random", requestOptions)
+                .then(response => response.json())
+                .then(data => contenido.innerHTML = data.value)
+                .catch(error => console.log('error', error));
+        }
+        else {
+            fetch("https://api.chucknorris.io/jokes/random?category=" + cate, requestOptions)
+                .then(response => response.json())
+                .then(data => contenido.innerHTML = data.value)
+                .catch(error => console.log('error', error));
+        }
+    });
+}
+if (botonbuscar && loquebuscas) {
+    const busqueda = encodeURIComponent(loquebuscas.value);
+    botonbuscar.addEventListener("click", () => {
+        fetch("https://api.chucknorris.io/jokes/search?query=" + busqueda, requestOptions)
+            .then(response => {
+            if (!response.ok) {
+                throw new Error('请求失败'); // 在HTTP请求返回错误状态时抛出一个错误
+            }
+            return response.json();
+        })
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     });
 }
